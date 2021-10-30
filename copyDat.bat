4c827d198c03f9197b5d18ff42a15817e54f2b22
Ben <van.citters.b@gmail.com>
2021-10-30 15:02:34 -0500

    script to copy the progs def

---
+++ copyDat.bat
@@ -0,0 +1,4 @@
+copy c:\Users\vanci\Documents\myMods\firstTry\progs.dat "c:\Users\vanci\Saved Games\Nightdive Studios\Quake\mpmod"\progs.dat
+copy c:\Users\vanci\Documents\myMods\firstTry\progs.dat "c:\Users\vanci\Saved Games\Nightdive Studios\Quake\Id1"\progs.dat
+timeout /T 2
+"c:\Program Files (x86)\Steam\steamapps\common\Quake\rerelease\Quake_x64_steam.exe"
